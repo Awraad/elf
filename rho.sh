c3cc30c77b38af823354456c5a31b40cd6b4a1cc
Awraad <46459701+Awraad@users.noreply.github.com>
2020-09-01 15:37:56 +0300

    Update and rename elf.sh to rho.sh

---
+++ rho.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+kubectl apply -f rho.namespace.yaml
+helm repo add elastic https://helm.elastic.co
+helm repo add fluent https://fluent.github.io/helm-charts
+helm repo update
+helm install elasticsearch elastic/elasticsearch --version=7.9.0 --namespace=rho
+helm install fluent-bit fluent/fluent-bit --namespace=rho
+helm install kibana elastic/kibana --version=7.9.0 --namespace=rho --set service.type=LoadBalancer
+kubectl run random-logger --image=chentex/random-logger -n rho
+kubectl apply -f ingress.yaml -n rho
