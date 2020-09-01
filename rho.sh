d3d2dbf447f0d2a8b055e43e7071655240d84944
Awraad <46459701+Awraad@users.noreply.github.com>
2020-09-01 16:10:20 +0300

    Update rho.sh

--- rho.sh
+++ rho.sh
@@ -4,8 +4,8 @@ kubectl apply -f elf.namespace.yaml
 helm repo add elastic https://helm.elastic.co
 helm repo add fluent https://fluent.github.io/helm-charts
 helm repo update
-helm install elasticsearch elastic/elasticsearch --version=7.9.0 --namespace=rho
-helm install fluent-bit fluent/fluent-bit --namespace=rho
-helm install kibana elastic/kibana --version=7.9.0 --namespace=rho --set service.type=LoadBalancer
-kubectl run random-logger --image=chentex/random-logger -n rho
-kubectl apply -f ingress.yaml -n rho
+helm install elasticsearch elastic/elasticsearch --version=7.9.0 --namespace=elf-rho
+helm install fluent-bit fluent/fluent-bit --namespace=elf-rho
+helm install kibana elastic/kibana --version=7.9.0 --namespace=elf-rho --set service.type=LoadBalancer
+kubectl run random-logger --image=chentex/random-logger -n elf-rho
+kubectl apply -f ingress.yaml -n elf-rho
